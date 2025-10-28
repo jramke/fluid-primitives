b45678ddf0e1a5953fd6055c84e67c569ec8c925
jramke <jstrmk@gmail.com>
2025-10-28 22:29:40 +0100

    chore: version

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Fluid Primitives',
     'description' => 'The headless component library for TYPO3 Fluid',
-    'version' => '0.2.2',
+    'version' => '0.2.3',
     'state' => 'beta',
     'author' => 'Joost Ramke',
     'author_email' => 'hey@joostramke.com',
