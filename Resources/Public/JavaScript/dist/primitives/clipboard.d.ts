432d810d9ddd66977e78a4a4f386a730fccadca3
Joost <jstrmk@gmail.com>
2025-11-01 23:12:54 +0100

    build

--- Resources/Public/JavaScript/dist/primitives/clipboard.d.ts
+++ Resources/Public/JavaScript/dist/primitives/clipboard.d.ts
@@ -1,12 +1,12 @@
 import { Component$1 as Component, Machine$1 as Machine } from "../index-C51Brj49.js";
-import * as _zag_js_types3 from "@zag-js/types";
+import * as _zag_js_types0 from "@zag-js/types";
 import * as clipboard from "@zag-js/clipboard";
 
 //#region Resources/Private/Primitives/Clipboard/Clipboard.d.ts
 declare class Clipboard extends Component<clipboard.Props, clipboard.Api> {
   name: string;
   initMachine(props: clipboard.Props): Machine<clipboard.Schema>;
-  initApi(): clipboard.Api<_zag_js_types3.PropTypes<{
+  initApi(): clipboard.Api<_zag_js_types0.PropTypes<{
     [x: string]: any;
   }>>;
   render(): void;
