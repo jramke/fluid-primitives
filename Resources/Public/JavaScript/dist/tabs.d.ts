a1b554ad531512ef56d1576608d501ddd759d0b0
Joost <jstrmk@gmail.com>
2025-11-02 23:12:09 +0100

    chore: version

--- Resources/Public/JavaScript/dist/tabs.d.ts
+++ Resources/Public/JavaScript/dist/tabs.d.ts
@@ -1,12 +1,12 @@
 import { Component$1 as Component, Machine$1 as Machine } from "./index-u51T4Hsb.js";
-import * as _zag_js_types5 from "@zag-js/types";
+import * as _zag_js_types4 from "@zag-js/types";
 import * as tabs from "@zag-js/tabs";
 
 //#region Resources/Private/Primitives/Tabs/Tabs.d.ts
 declare class Tabs extends Component<tabs.Props, tabs.Api> {
   name: string;
   initMachine(props: tabs.Props): Machine<any>;
-  initApi(): tabs.Api<_zag_js_types5.PropTypes<{
+  initApi(): tabs.Api<_zag_js_types4.PropTypes<{
     [x: string]: any;
   }>>;
   render: () => void;
