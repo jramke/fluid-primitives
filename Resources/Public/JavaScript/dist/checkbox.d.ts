9f4777d296b4ed7efcd81b4ff617689b3ae47af9
Joost <jstrmk@gmail.com>
2025-12-30 00:00:52 +0100

    chore: version

--- Resources/Public/JavaScript/dist/checkbox.d.ts
+++ Resources/Public/JavaScript/dist/checkbox.d.ts
@@ -1,5 +1,5 @@
 import { FieldAwareComponent$1 as FieldAwareComponent, FieldMachine, Machine$1 as Machine } from "./index-iJSgk-K3.js";
-import * as _zag_js_types2 from "@zag-js/types";
+import * as _zag_js_types0 from "@zag-js/types";
 import * as checkbox from "@zag-js/checkbox";
 
 //#region Resources/Private/Primitives/Checkbox/Checkbox.d.ts
@@ -7,7 +7,7 @@ declare class Checkbox extends FieldAwareComponent<checkbox.Props, checkbox.Api>
   static name: string;
   propsWithField(props: checkbox.Props, fieldMachine: FieldMachine): checkbox.Props;
   initMachine(props: checkbox.Props): Machine<any>;
-  initApi(): checkbox.Api<_zag_js_types2.PropTypes<{
+  initApi(): checkbox.Api<_zag_js_types0.PropTypes<{
     [x: string]: any;
   }>>;
   render(): void;
