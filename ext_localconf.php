5eea2511d5eff1ce80b3e505aa1e5ef1144d96ce
jramke <jstrmk@gmail.com>
2026-01-07 21:13:04 +0100

    feat: exclude some props for ext:storybook

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,8 @@
 
 declare(strict_types=1);
 
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 defined('TYPO3') || die();
 
@@ -12,4 +13,17 @@ if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['ui'])) {
 }
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['ui'][] = 'Jramke\\FluidPrimitives\\ViewHelpers';
 
+// Register primitives namespace
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['primitives'] = ['Jramke\\FluidPrimitives\\Component\\ComponentPrimitivesCollection'];
+
+// Exclude specific arguments from storybook controls when using EXT:storybook
+if (ExtensionManagementUtility::isLoaded('storybook')) {
+    $existing = $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['storybook']['excludeArguments'] ?? '';
+    $existingArr = GeneralUtility::trimExplode(',', $existing, true);
+
+    $addList = ['ids', 'attributes', 'asChild', 'rootId', 'controlled', 'spreadProps'];
+
+    $merged = array_values(array_unique(array_merge($existingArr, $addList)));
+
+    $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['storybook']['excludeArguments'] = implode(',', $merged);
+}
