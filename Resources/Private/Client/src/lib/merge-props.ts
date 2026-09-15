7898e593d86dd9af5c1e2f8af354f2e0c5430396
jramke <jstrmk@gmail.com>
2026-09-15 17:51:35 +0200

    chore: upgrade zag.js to 1.44.0, drop local spreadProps/normalizeProps/mergeProps workarounds
    The upstream style-patching fix (chakra-ui/zag@83afd32) these files were
    temporarily copied from has now shipped in @zag-js/vanilla, so re-export from
    there again instead of carrying local copies.

--- Resources/Private/Client/src/lib/merge-props.ts
+++ Resources/Private/Client/src/lib/merge-props.ts
@@ -1,4 +1,1 @@
-// export { mergeProps } from '@zag-js/vanilla';
-// TODO: temporarily copied from @zag-js/vanilla until https://github.com/chakra-ui/zag/commit/83afd32b2037c36abf431368f0a8d2f4c429bc58 is released.
-
-export { mergeProps } from '@zag-js/core';
+export { mergeProps } from '@zag-js/vanilla';
